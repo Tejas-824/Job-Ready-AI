1f35fecb0515d3d2e14b477a7a81081475d34858
Tejas-824 <tejaswinigmo@gmail.com>
2026-04-10 11:26:36 +0530

    fix: changed Gemini version

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -50,7 +50,7 @@ Job Description: ${jobDescription}
 `
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash-lite",
+        model: "gemini-1.5-flash",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
